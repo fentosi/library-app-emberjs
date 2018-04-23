cd1b3b8de865240453edf25662a83206c4b4bb65
fentosi <fentosi.adam@gmail.com>
2018-04-23 20:13:01 +1200

    Check already exists email for invitation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,13 +7,24 @@ export default Controller.extend({
     isDisabled: not('hasValidEmail'),
     actions: {
         saveInvitation (invitationModel) {
-            invitationModel.save().then((savedInvitation) => {
-                this.set('feedbackMessage', `Your email address has been saved with id: ${savedInvitation.id}`);
-                
-                const newInvitation = this.store.createRecord('invitation');
+            this.get('store').query('invitation', {
+                filter: {
+                    email: invitationModel.email
+                }
+            }).then((invitations) => {
+                if (invitations.length == 1) {
+                    invitationModel.save().then((savedInvitation) => {
+                        this.set('feedbackMessage', `Your email address has been saved with id: ${savedInvitation.id}`);
+                        
+                        const newInvitation = this.store.createRecord('invitation');
+        
+                        this.set('model', newInvitation);
+                    });        
+                } else {
+                    this.set('errorMessage', `You already subscribed with email: ${invitationModel.email}`);
+                }
+            })
 
-                this.set('model', newInvitation);
-            });
         }
     }
 });
