5e26463942ae6fd045f0c7e1f69dce96ff40d6fc
fentosi <fentosi.adam@gmail.com>
2018-05-13 16:04:16 +1200

    Update on contact us form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,24 +1,14 @@
 import Controller from '@ember/controller';
-import {match, gte, and, not} from '@ember/object/computed';
+import {match, gte, and, not, or} from '@ember/object/computed';
 
 export default Controller.extend({
+    isLoading: false,
     emailAddress: "",
     text: "",
     messageSent: false,
     hasValidEmail: match('model.email', /^.+@.+\..+$/),
     hasText: gte('model.text.length', 5),
     isValid: and('hasValidEmail', 'hasText'),
-    isDisabled: not('isValid'),
-    actions: {
-        saveContact(contactModel) {
-            contactModel.save().then(() => {
-                const newContact = this.store.createRecord('contact');
-                
-                this.set('messageSent', true);
-                this.set('feedbackMessage', `Thank you for your messge. We will be in touch with you shortly.`);
-
-                this.set('model', newContact);
-            });
-        }
-    }
+    isNotValid: not('isValid'),
+    isDisabled: or('isNotValid', 'isLoading')
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -3,5 +3,31 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model() {
         return this.store.createRecord('contact');
+    },
+    actions: {
+        willTransition(transition) {
+            let model = this.controller.get('model');
+
+            if (Object.keys(model.changedAttributes()).length > 0) {
+                if (confirm("Are you sure you want to leave the page?")) {
+                    model.rollbackAttributes();
+                } else {
+                    transition.abort();
+                }
+            }
+        },
+        saveContact(contactModel) {
+            this.controller.set('isLoading', true);
+            contactModel.save().then(() => {
+                const newContact = this.store.createRecord('contact');
+                
+                this.controller.set('messageSent', true);
+                this.controller.set('feedbackMessage', `Thank you for your messge. We will be in touch with you shortly.`);
+
+                this.controller.set('model', newContact);
+
+                this.controller.set('isLoading', false);
+            });
+        }
     }
 });
