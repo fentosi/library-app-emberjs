ef7ff515e1b589b5d43a0f6c31c7fcbfd1507099
fentosi <fentosi.adam@gmail.com>
2018-05-13 14:51:05 +1200

    Add admin/invitations page

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model() {
         return this.store.createRecord('invitation');
+    },
+    actions: {
+        willTransition() {
+            this.controller.get('model').rollbackAttributes();
+        }
     }
 });
